1f7f4dd80359238a39467a7434e4057bdcfbeac0
SyncMr <talk2pragadheesh@gmail.com>
2016-12-27 07:56:51 -0800

    sample java file

--- b.java
+++ b.java
@@ -6,8 +6,5 @@ public class b
     public static void main(String[] args)
     {
         System.out.println("hi");
-        int[] A = {-1, 3, -4, 5, 1, -6, 2, 1};
-        int[] A = {-1, 3, -4, 5, 1, -6, 2, 1};
-        System.out.println(solution(A));
     }
 }
