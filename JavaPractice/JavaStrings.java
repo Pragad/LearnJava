39887707805636250935f744c5e64bdde8b992f5
Pragad <talk2pragadheesh@gmail.com>
2017-01-27 11:46:34 -0800

    Java Split Strings

---
+++ JavaPractice/JavaStrings.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+// https://www.tutorialspoint.com/java/lang/string_split.htm
+public class JavaStrings
+{
+    public static void main(String[] args) {
+        String str = "a d, m, i.n";
+        String[] tokens = str.split(",\\s*");
+        //String[] tokens = str.split(",\\s*");
+
+        for (String tok : tokens) {
+            System.out.println(tok);
+        }
+    }
+}
+
+/*
+https://books.google.com/books?id=diqHjRjMhW0C&pg=PA24&lpg=PA24&dq=java+string+split+%22,%5C%5Cs*%22&source=bl&ots=IwpwyEfgmz&sig=n0PZCuhVxDx5pwgEZI1D_YpTXrE&hl=en&sa=X&ved=0ahUKEwi5g76FuOHRAhVO3WMKHVd7C_UQ6AEISTAI#v=onepage&q=java%20string%20split%20%22%2C%5C%5Cs*%22&f=false
+\s - Whitespace (space, tab, line feed, carriage return)
+\S - Non Whitespace
+\d - Digits
+\D - Non Digits
+\w - 0-9,a-z,A-Z,_
+\W - Non word chars
+*  - Repeat preceding pattern 0 or more times
++  - Repeat preceding pattern 1 or more times
+?  - Repeat preceding pattern 0 or 1 times
+*/
--- b.java
+++ b.java
@@ -3,8 +3,14 @@ import java.util.*;
 
 public class b
 {
+    public static void foo()
+    {
+        System.out.println("hello");
+    }
+
     public static void main(String[] args)
     {
         System.out.println("hi");
+        foo();
     }
 }
