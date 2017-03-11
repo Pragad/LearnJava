00ed9afcab2494753df715ec479f9ca5299fe9d1
Pragad <talk2pragadheesh@gmail.com>
2017-03-11 10:23:30 -0800

    Computing MD5 hash of file

--- JavaPractice/MD5OfFile.java
+++ JavaPractice/MD5OfFile.java
@@ -2,7 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class MD5OfFile {
+    public static String getMD5OfFile(String fileName) {
+        FileInputStream fis = new FileInputStream(new File(fileName));
+        String md5 = org.apache.commons.codec.digest.DigestUtils.md5Hex(fis);
+        fis.close();
+        return md5;
+    }
+
     public static void main(String[] args) {
-        Sytem.out.println();
+        System.out.println();
+        String file = "/Users/pragad/ItsMine/Dev_Git/LearnJava/JavaPractice/a.txt";
+        String md5Value = getMD5OfFile(file);
     }
 }
